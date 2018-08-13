b4e61bdd990d07e026ca6f15aaf1220b2e797dc6
Việt Nguyễn <nguyenviet@vietnguyen.local>
2018-08-13 13:58:08 +0700

    Initial Commit

---
+++ JsonSampleDemoTests/JsonSampleDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  JsonSampleDemoTests.swift
+//  JsonSampleDemoTests
+//
+//  Created by Nguyễn Việt on 8/13/18.
+//  Copyright © 2018 Việt Nguyễn. All rights reserved.
+//
+
+import XCTest
+@testable import JsonSampleDemo
+
+class JsonSampleDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
